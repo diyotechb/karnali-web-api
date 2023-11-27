7394cfd8e481f665efddda4081ecd8ff93880f7b
Bishwo Bhandari <bishwo@diyotech.net>
2023-11-27 09:43:57 -0500

    adding hello

--- src/main/java/com/example/starter/StarterApplication.java
+++ src/main/java/com/example/starter/StarterApplication.java
@@ -2,12 +2,26 @@ package com.example.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class StarterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarterApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigure(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
