d9164ceaf7b460122b32dd0a79056d284dcd99cf
Sudip Adhikari <shudip79@gmail.com>
2023-12-06 14:43:04 -0500

    Added Address (create, update and delete) while Business (create, update and delete)

--- src/main/java/com/example/starter/business/repository/AddressRepository.java
+++ src/main/java/com/example/starter/business/repository/AddressRepository.java
@@ -1,7 +1,6 @@
 package com.example.starter.business.repository;
 
 import com.example.starter.business.entity.Address;
-import com.example.starter.business.entity.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/example/starter/business/repository/BusinessTypeRepository.java
+++ src/main/java/com/example/starter/business/repository/BusinessTypeRepository.java
@@ -1,6 +1,5 @@
 package com.example.starter.business.repository;
 
-import com.example.starter.business.entity.Business;
 import com.example.starter.business.entity.BusinessType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
